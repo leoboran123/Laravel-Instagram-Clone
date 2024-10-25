914861cc3fcd773a481f9377dbd22fa2a2654f8e
leoboran123 <erdem_boran_badd@hotmail.com>
2024-10-25 22:42:31 +0300

    last commit

--- app/Profile.php
+++ app/Profile.php
@@ -23,7 +23,7 @@ class Profile extends Model
     
 
     public function user(){
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class)->withPivot('id');
     }
 
     public function followers(){
